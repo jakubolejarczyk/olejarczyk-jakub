bd714954c8d838f43ae455ed57678c2963158c9e
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2026-03-07 00:07:00 +0100

    Clean up the data

--- src/app/data/model/experience-data.model.ts
+++
@@ -1,9 +0,0 @@
-import { CompanyData } from './company-data.model';
-
-export namespace ExperienceData {
-  interface Model {
-    company: CompanyData.Model;
-  }
-
-  export type Type = Model[];
-}
