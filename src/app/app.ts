0c8cd272b8149cc07728ace4ff20dbc8971ab306
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2026-03-13 15:10:56 +0100

    Create header, main and footer components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './component/header/header.component';
+import { MainComponent } from './component/main/main.component';
+import { FooterComponent } from './component/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
+  imports: [HeaderComponent, MainComponent, FooterComponent],
 })
 export class App {}
---
+++ src/app/component/main/main.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'main-component',
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.scss',
+})
+export class MainComponent {}
