5731462e415d51bbd633b6d78b8b57a873520593
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2026-03-14 23:57:11 +0100

    Move all components to the view folder

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './config/app.config';
-import { App } from './app/app';
+import { App } from './view/app';
 
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
