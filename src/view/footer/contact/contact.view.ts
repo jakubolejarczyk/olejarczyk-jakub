0f6587dffc2285f060266ee47de880c591c60293
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2026-03-17 17:40:53 +0100

    Clean up the section

--- src/view/footer/contact/contact.view.ts
+++ src/view/footer/contact/contact.view.ts
@@ -1,28 +1,9 @@
 import { Component } from '@angular/core';
-import { TextComponent } from '../../../component/text/text.component';
 import { StoreCore } from '../../../core/store.core';
-import { LinkComponent } from '../../../component/link/link.component';
 
 @Component({
   selector: 'contact-view',
   templateUrl: './contact.view.html',
   styleUrl: './contact.view.scss',
-  imports: [TextComponent, LinkComponent],
 })
-export class ContactView extends StoreCore {
-  fullname = this.store.selectSignal((state) => {
-    const { firstname, lastname } = state.personal;
-    return `${firstname} ${lastname}`;
-  });
-
-  socialMedias = this.store.selectSignal((state) => state.personal.socialMedias);
-
-  email = this.store.selectSignal((state) => state.personal.email);
-
-  phone = this.store.selectSignal((state) => state.personal.phone);
-
-  location = this.store.selectSignal((state) => {
-    const { country, city } = state.personal;
-    return `${country}, ${city}`;
-  });
-}
+export class ContactView extends StoreCore {}
