a0fc7d303c79f54c55090e96b1c66bfb788d4774
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2026-03-17 13:36:41 +0100

    Create basic contact serction

--- src/view/footer/footer.view.stories.ts
+++ src/view/footer/footer.view.stories.ts
@@ -1,10 +1,11 @@
 import { Meta, StoryObj } from '@storybook/angular';
-
 import { FooterView } from './footer.view';
+import { StorybookUtils } from '../../utils/storybook.utils';
 
 const meta: Meta<FooterView> = {
   title: 'View/Footer',
   component: FooterView,
+  decorators: StorybookUtils.getDecorators(),
 };
 export default meta;
 type Story = StoryObj<FooterView>;
