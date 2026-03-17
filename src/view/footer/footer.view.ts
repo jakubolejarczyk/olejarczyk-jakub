ce795d49b45d82cfd7226fde93f2e63434f2793c
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2026-03-17 17:24:01 +0100

    Refactor the footer html structure

--- src/view/footer/footer.view.ts
+++ src/view/footer/footer.view.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { CopyrightView } from './copyright/copyright.view';
 import { ContactView } from './contact/contact.view';
+import { CopyrightView } from './copyright/copyright.view';
 
 @Component({
   selector: 'footer-view',
   templateUrl: './footer.view.html',
   styleUrl: './footer.view.scss',
-  imports: [CopyrightView, ContactView],
+  imports: [ContactView, CopyrightView],
 })
 export class FooterView {}
