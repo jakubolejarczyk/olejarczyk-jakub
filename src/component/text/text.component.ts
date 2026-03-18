759932e3d434bcaafb7ab899c112866131a1b030
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2026-03-18 15:36:18 +0100

    Add footer styles

--- src/component/text/text.component.ts
+++ src/component/text/text.component.ts
@@ -1,13 +1,21 @@
 import { Component, input } from '@angular/core';
+import { NgClass } from '@angular/common';
 import { TextType } from './text.type';
 
 @Component({
   selector: 'text-component',
   templateUrl: './text.component.html',
   styleUrl: './text.component.scss',
+  imports: [NgClass],
 })
 export class TextComponent {
   type = input.required<TextType>();
 
   value = input.required<string>();
+
+  isLink = input(false);
+
+  getTextClassName() {
+    return this.isLink() ? 'text_for_link' : 'text';
+  }
 }
