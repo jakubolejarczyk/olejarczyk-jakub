6f78fe74af4f393e0d94ffad2ddca71e581908af
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2026-03-19 12:43:08 +0100

    Remove styles from main view

--- src/view/main/main.view.ts
+++ src/view/main/main.view.ts
@@ -4,7 +4,6 @@ import { EducationView } from './education/education.view';
 @Component({
   selector: 'main-view',
   templateUrl: './main.view.html',
-  styleUrl: './main.view.scss',
   imports: [EducationView],
 })
 export class MainView {}
