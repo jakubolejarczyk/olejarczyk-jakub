b160f11492d529aeb79ee9007cb3f063e2f67672
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2026-03-20 00:38:58 +0100

    Create basic loop

--- src/view/view.ts
+++ src/view/view.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
+import { StoreCore } from '../store/store.core';
 
 @Component({
   selector: 'view',
   templateUrl: './view.html',
 })
-export class View {}
+export class View extends StoreCore {
+  group = this.store.selectSignal((state) => {
+    return Object.values(state.group).sort((prev, next) => prev.order - next.order);
+  });
+}
