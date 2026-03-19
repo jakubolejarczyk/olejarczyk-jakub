09bd7f99e3974af8e214c7646b557e00e1e9b1b2
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2026-03-19 13:47:11 +0100

    Refactor the view component

--- src/view/header/header.view.ts
+++ src/view/header/header.view.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
 
-import { HomeView } from './home/home.view';
-import { NavigationView } from './navigation/navigation.view';
-
 @Component({
   selector: 'header-view',
   templateUrl: './header.view.html',
   styleUrl: './header.view.scss',
-  imports: [HomeView, NavigationView],
 })
 export class HeaderView {}
--- src/view/view.ts
+++ src/view/view.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FooterView } from './footer/footer.view';
 import { MainView } from './main/main.view';
+import { HeaderView } from './header/header.view';
 
 @Component({
   selector: 'view',
   templateUrl: './view.html',
-  imports: [FooterView, MainView],
+  imports: [FooterView, MainView, HeaderView],
 })
 export class View {}
