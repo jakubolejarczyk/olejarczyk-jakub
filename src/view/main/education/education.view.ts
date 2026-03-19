a0aac5ec9d17c7259ccadec206f8c492679bb86f
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2026-03-19 20:21:13 +0100

    Create the education section

--- src/view/main/education/education.view.ts
+++ src/view/main/education/education.view.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { StoreCore } from '../../../core/store.core';
+import { TextComponent } from '../../../component/text/text.component';
 
 @Component({
   selector: 'education-view',
   templateUrl: './education.view.html',
   styleUrl: './education.view.scss',
+  imports: [TextComponent],
 })
 export class EducationView extends StoreCore {
   education = this.store.selectSignal((state) => state.education);
