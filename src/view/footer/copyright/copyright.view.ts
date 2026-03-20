72b79a29a4423b94ec9c980775d8663ceea3be01
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2026-03-20 18:20:19 +0100

    Create copyright

--- src/view/footer/copyright/copyright.view.ts
+++ src/view/footer/copyright/copyright.view.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
+import { TextComponent } from '../../../component/text/text.component';
 import { StoreCore } from '../../../store/store.core';
 import { DateUtil } from '../../../util/date.util';
-import { TextComponent } from '../../../component/text/text.component';
 
 @Component({
   selector: 'copyright-view',
   templateUrl: './copyright.view.html',
+  styleUrl: './copyright.view.scss',
   imports: [TextComponent],
 })
 export class CopyrightView extends StoreCore {
   yearAndFullname = this.store.selectSignal((state) => {
     const { firstname, lastname } = state.personal;
     const year = DateUtil.getYear();
-    return `${year} ${firstname} ${lastname}`;
+    return `${year} ${firstname} ${lastname}.`;
   });
 }
