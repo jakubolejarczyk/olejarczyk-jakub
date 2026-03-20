5eb16fb2a4623236602a7237f14ab509ca75789d
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2026-03-20 15:34:42 +0100

    Remove unused scss file

--- src/view/main/education/education.view.ts
+++ src/view/main/education/education.view.ts
@@ -3,6 +3,5 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'education-view',
   templateUrl: './education.view.html',
-  styleUrl: './education.view.scss',
 })
 export class EducationView {}
