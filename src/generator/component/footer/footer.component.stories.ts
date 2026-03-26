d1fbfed00e7579a207e28a5de8bfefd76075581d
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2026-03-26 13:16:56 +0100

    Create generator components

---
+++ src/generator/component/footer/footer.component.stories.ts
@@ -0,0 +1,11 @@
+import { Meta, StoryObj } from '@storybook/angular';
+import { FooterComponent } from './footer.component';
+
+const meta: Meta<FooterComponent> = {
+  title: 'generator/component/footer',
+  component: FooterComponent,
+};
+export default meta;
+type Story = StoryObj<FooterComponent>;
+
+export const Primary: Story = {};
---
+++ src/generator/component/header/header.component.stories.ts
@@ -0,0 +1,11 @@
+import { Meta, StoryObj } from '@storybook/angular';
+import { HeaderComponent } from './header.component';
+
+const meta: Meta<HeaderComponent> = {
+  title: 'generator/component/header',
+  component: HeaderComponent,
+};
+export default meta;
+type Story = StoryObj<HeaderComponent>;
+
+export const Primary: Story = {};
