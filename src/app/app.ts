27f62326fd6c3e9d243af50f0bcd2ddd19318501
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2026-04-02 23:12:41 +0200

    Add template texts to the components

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,6 +6,7 @@ import { HeaderLayoutBuilder } from '../generator/builder/layout/header-layout.b
 import { MainLayoutBuilder } from '../generator/builder/layout/main-layout.builder';
 import { FooterLayoutBuilder } from '../generator/builder/layout/footer-layout.builder';
 import { SectionLayoutBuilder } from '../generator/builder/layout/section-layout.builder';
+import { TextControlBuilder } from '../generator/builder/control/text-control.builder';
 
 @Component({
   selector: 'app',
@@ -16,21 +17,54 @@ export class App {
   nodes: NodeModel[] = [
     RootLayoutBuilder.build([
       HeaderLayoutBuilder.build([
-        SectionLayoutBuilder.build('navigation', []),
-        SectionLayoutBuilder.build('home', []),
+        SectionLayoutBuilder.build('navigation', [
+          TextControlBuilder.build('h1', 'Header 1'),
+          TextControlBuilder.build('p', 'Navigation'),
+        ]),
+        SectionLayoutBuilder.build('home', [
+          TextControlBuilder.build('h2', 'Header 2'),
+          TextControlBuilder.build('p', 'Home'),
+        ]),
       ]),
       MainLayoutBuilder.build([
-        SectionLayoutBuilder.build('about-me', []),
-        SectionLayoutBuilder.build('technologies', []),
-        SectionLayoutBuilder.build('skills', []),
-        SectionLayoutBuilder.build('experience', []),
-        SectionLayoutBuilder.build('resume', []),
-        SectionLayoutBuilder.build('projects', []),
-        SectionLayoutBuilder.build('education', []),
+        SectionLayoutBuilder.build('about-me', [
+          TextControlBuilder.build('h3', 'Header 3'),
+          TextControlBuilder.build('p', 'About me'),
+        ]),
+        SectionLayoutBuilder.build('technologies', [
+          TextControlBuilder.build('h3', 'Header 3'),
+          TextControlBuilder.build('p', 'Technologies'),
+        ]),
+        SectionLayoutBuilder.build('skills', [
+          TextControlBuilder.build('h3', 'Header 3'),
+          TextControlBuilder.build('p', 'Skills'),
+        ]),
+        SectionLayoutBuilder.build('experience', [
+          TextControlBuilder.build('h3', 'Header 3'),
+          TextControlBuilder.build('p', 'Experience'),
+        ]),
+        SectionLayoutBuilder.build('resume', [
+          TextControlBuilder.build('h3', 'Header 3'),
+          TextControlBuilder.build('p', 'Resume'),
+        ]),
+        SectionLayoutBuilder.build('projects', [
+          TextControlBuilder.build('h3', 'Header 3'),
+          TextControlBuilder.build('p', 'Projects'),
+        ]),
+        SectionLayoutBuilder.build('education', [
+          TextControlBuilder.build('h3', 'Header 3'),
+          TextControlBuilder.build('p', 'Education'),
+        ]),
       ]),
       FooterLayoutBuilder.build([
-        SectionLayoutBuilder.build('contact', []),
-        SectionLayoutBuilder.build('copyright', []),
+        SectionLayoutBuilder.build('contact', [
+          TextControlBuilder.build('h3', 'Header 3'),
+          TextControlBuilder.build('p', 'Contact'),
+        ]),
+        SectionLayoutBuilder.build('copyright', [
+          TextControlBuilder.build('h3', 'Header 3'),
+          TextControlBuilder.build('p', 'Copyright'),
+        ]),
       ]),
     ]),
   ];
