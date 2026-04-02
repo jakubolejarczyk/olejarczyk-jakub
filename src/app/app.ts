7809753a4ab45d4b33f0af59373618f1233a214d
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2026-04-02 22:56:35 +0200

    Create basic structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,7 +6,6 @@ import { HeaderLayoutBuilder } from '../generator/builder/layout/header-layout.b
 import { MainLayoutBuilder } from '../generator/builder/layout/main-layout.builder';
 import { FooterLayoutBuilder } from '../generator/builder/layout/footer-layout.builder';
 import { SectionLayoutBuilder } from '../generator/builder/layout/section-layout.builder';
-import { TextControlBuilder } from '../generator/builder/control/text-control.builder';
 
 @Component({
   selector: 'app',
@@ -15,155 +14,23 @@ import { TextControlBuilder } from '../generator/builder/control/text-control.bu
 })
 export class App {
   nodes: NodeModel[] = [
-    RootLayoutBuilder.build('primary', [
-      HeaderLayoutBuilder.build('primary', [
-        SectionLayoutBuilder.build('primary', 'navigation', [
-          TextControlBuilder.build('primary', 'Header 1', 'h1'),
-          TextControlBuilder.build(
-            'primary',
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ornare sit amet justo quis porttitor. Vivamus volutpat, velit in cursus tempor, enim lectus viverra augue, non bibendum leo nisl non arcu. Aenean dapibus metus tellus, at viverra ipsum posuere quis. Sed diam enim, sollicitudin at orci id, venenatis fringilla neque. Etiam pellentesque eget purus eu ultricies. Praesent tristique elit mauris, nec rutrum elit condimentum et. Ut vel lectus eros. Morbi auctor sit amet leo eu lacinia. Duis mattis quam non consequat porttitor. Sed sodales sit amet libero vitae eleifend.',
-            'p',
-          ),
-          TextControlBuilder.build(
-            'primary',
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ornare sit amet justo quis porttitor. Vivamus volutpat, velit in cursus tempor, enim lectus viverra augue, non bibendum leo nisl non arcu. Aenean dapibus metus tellus, at viverra ipsum posuere quis. Sed diam enim, sollicitudin at orci id, venenatis fringilla neque. Etiam pellentesque eget purus eu ultricies. Praesent tristique elit mauris, nec rutrum elit condimentum et. Ut vel lectus eros. Morbi auctor sit amet leo eu lacinia. Duis mattis quam non consequat porttitor. Sed sodales sit amet libero vitae eleifend.',
-            'small',
-          ),
-        ]),
-        SectionLayoutBuilder.build('primary', 'home', [
-          TextControlBuilder.build('primary', 'Header 2', 'h2'),
-          TextControlBuilder.build(
-            'primary',
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ornare sit amet justo quis porttitor. Vivamus volutpat, velit in cursus tempor, enim lectus viverra augue, non bibendum leo nisl non arcu. Aenean dapibus metus tellus, at viverra ipsum posuere quis. Sed diam enim, sollicitudin at orci id, venenatis fringilla neque. Etiam pellentesque eget purus eu ultricies. Praesent tristique elit mauris, nec rutrum elit condimentum et. Ut vel lectus eros. Morbi auctor sit amet leo eu lacinia. Duis mattis quam non consequat porttitor. Sed sodales sit amet libero vitae eleifend.',
-            'p',
-          ),
-          TextControlBuilder.build(
-            'primary',
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ornare sit amet justo quis porttitor. Vivamus volutpat, velit in cursus tempor, enim lectus viverra augue, non bibendum leo nisl non arcu. Aenean dapibus metus tellus, at viverra ipsum posuere quis. Sed diam enim, sollicitudin at orci id, venenatis fringilla neque. Etiam pellentesque eget purus eu ultricies. Praesent tristique elit mauris, nec rutrum elit condimentum et. Ut vel lectus eros. Morbi auctor sit amet leo eu lacinia. Duis mattis quam non consequat porttitor. Sed sodales sit amet libero vitae eleifend.',
-            'small',
-          ),
-        ]),
+    RootLayoutBuilder.build([
+      HeaderLayoutBuilder.build([
+        SectionLayoutBuilder.build('navigation', []),
+        SectionLayoutBuilder.build('home', []),
       ]),
-      MainLayoutBuilder.build('primary', [
-        SectionLayoutBuilder.build('primary', 'about-me', [
-          TextControlBuilder.build('primary', 'Header 3', 'h3'),
-          TextControlBuilder.build(
-            'primary',
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ornare sit amet justo quis porttitor. Vivamus volutpat, velit in cursus tempor, enim lectus viverra augue, non bibendum leo nisl non arcu. Aenean dapibus metus tellus, at viverra ipsum posuere quis. Sed diam enim, sollicitudin at orci id, venenatis fringilla neque. Etiam pellentesque eget purus eu ultricies. Praesent tristique elit mauris, nec rutrum elit condimentum et. Ut vel lectus eros. Morbi auctor sit amet leo eu lacinia. Duis mattis quam non consequat porttitor. Sed sodales sit amet libero vitae eleifend.',
-            'p',
-          ),
-          TextControlBuilder.build(
-            'primary',
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ornare sit amet justo quis porttitor. Vivamus volutpat, velit in cursus tempor, enim lectus viverra augue, non bibendum leo nisl non arcu. Aenean dapibus metus tellus, at viverra ipsum posuere quis. Sed diam enim, sollicitudin at orci id, venenatis fringilla neque. Etiam pellentesque eget purus eu ultricies. Praesent tristique elit mauris, nec rutrum elit condimentum et. Ut vel lectus eros. Morbi auctor sit amet leo eu lacinia. Duis mattis quam non consequat porttitor. Sed sodales sit amet libero vitae eleifend.',
-            'small',
-          ),
-        ]),
-        SectionLayoutBuilder.build('primary', 'technologies', [
-          TextControlBuilder.build('primary', 'Header 3', 'h3'),
-          TextControlBuilder.build(
-            'primary',
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ornare sit amet justo quis porttitor. Vivamus volutpat, velit in cursus tempor, enim lectus viverra augue, non bibendum leo nisl non arcu. Aenean dapibus metus tellus, at viverra ipsum posuere quis. Sed diam enim, sollicitudin at orci id, venenatis fringilla neque. Etiam pellentesque eget purus eu ultricies. Praesent tristique elit mauris, nec rutrum elit condimentum et. Ut vel lectus eros. Morbi auctor sit amet leo eu lacinia. Duis mattis quam non consequat porttitor. Sed sodales sit amet libero vitae eleifend.',
-            'p',
-          ),
-          TextControlBuilder.build(
-            'primary',
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ornare sit amet justo quis porttitor. Vivamus volutpat, velit in cursus tempor, enim lectus viverra augue, non bibendum leo nisl non arcu. Aenean dapibus metus tellus, at viverra ipsum posuere quis. Sed diam enim, sollicitudin at orci id, venenatis fringilla neque. Etiam pellentesque eget purus eu ultricies. Praesent tristique elit mauris, nec rutrum elit condimentum et. Ut vel lectus eros. Morbi auctor sit amet leo eu lacinia. Duis mattis quam non consequat porttitor. Sed sodales sit amet libero vitae eleifend.',
-            'small',
-          ),
-        ]),
-        SectionLayoutBuilder.build('primary', 'skills', [
-          TextControlBuilder.build('primary', 'Header 3', 'h3'),
-          TextControlBuilder.build(
-            'primary',
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ornare sit amet justo quis porttitor. Vivamus volutpat, velit in cursus tempor, enim lectus viverra augue, non bibendum leo nisl non arcu. Aenean dapibus metus tellus, at viverra ipsum posuere quis. Sed diam enim, sollicitudin at orci id, venenatis fringilla neque. Etiam pellentesque eget purus eu ultricies. Praesent tristique elit mauris, nec rutrum elit condimentum et. Ut vel lectus eros. Morbi auctor sit amet leo eu lacinia. Duis mattis quam non consequat porttitor. Sed sodales sit amet libero vitae eleifend.',
-            'p',
-          ),
-          TextControlBuilder.build(
-            'primary',
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ornare sit amet justo quis porttitor. Vivamus volutpat, velit in cursus tempor, enim lectus viverra augue, non bibendum leo nisl non arcu. Aenean dapibus metus tellus, at viverra ipsum posuere quis. Sed diam enim, sollicitudin at orci id, venenatis fringilla neque. Etiam pellentesque eget purus eu ultricies. Praesent tristique elit mauris, nec rutrum elit condimentum et. Ut vel lectus eros. Morbi auctor sit amet leo eu lacinia. Duis mattis quam non consequat porttitor. Sed sodales sit amet libero vitae eleifend.',
-            'small',
-          ),
-        ]),
-        SectionLayoutBuilder.build('primary', 'experience', [
-          TextControlBuilder.build('primary', 'Header 3', 'h3'),
-          TextControlBuilder.build(
-            'primary',
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ornare sit amet justo quis porttitor. Vivamus volutpat, velit in cursus tempor, enim lectus viverra augue, non bibendum leo nisl non arcu. Aenean dapibus metus tellus, at viverra ipsum posuere quis. Sed diam enim, sollicitudin at orci id, venenatis fringilla neque. Etiam pellentesque eget purus eu ultricies. Praesent tristique elit mauris, nec rutrum elit condimentum et. Ut vel lectus eros. Morbi auctor sit amet leo eu lacinia. Duis mattis quam non consequat porttitor. Sed sodales sit amet libero vitae eleifend.',
-            'p',
-          ),
-          TextControlBuilder.build(
-            'primary',
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ornare sit amet justo quis porttitor. Vivamus volutpat, velit in cursus tempor, enim lectus viverra augue, non bibendum leo nisl non arcu. Aenean dapibus metus tellus, at viverra ipsum posuere quis. Sed diam enim, sollicitudin at orci id, venenatis fringilla neque. Etiam pellentesque eget purus eu ultricies. Praesent tristique elit mauris, nec rutrum elit condimentum et. Ut vel lectus eros. Morbi auctor sit amet leo eu lacinia. Duis mattis quam non consequat porttitor. Sed sodales sit amet libero vitae eleifend.',
-            'small',
-          ),
-        ]),
-        SectionLayoutBuilder.build('primary', 'resume', [
-          TextControlBuilder.build('primary', 'Header 3', 'h3'),
-          TextControlBuilder.build(
-            'primary',
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ornare sit amet justo quis porttitor. Vivamus volutpat, velit in cursus tempor, enim lectus viverra augue, non bibendum leo nisl non arcu. Aenean dapibus metus tellus, at viverra ipsum posuere quis. Sed diam enim, sollicitudin at orci id, venenatis fringilla neque. Etiam pellentesque eget purus eu ultricies. Praesent tristique elit mauris, nec rutrum elit condimentum et. Ut vel lectus eros. Morbi auctor sit amet leo eu lacinia. Duis mattis quam non consequat porttitor. Sed sodales sit amet libero vitae eleifend.',
-            'p',
-          ),
-          TextControlBuilder.build(
-            'primary',
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ornare sit amet justo quis porttitor. Vivamus volutpat, velit in cursus tempor, enim lectus viverra augue, non bibendum leo nisl non arcu. Aenean dapibus metus tellus, at viverra ipsum posuere quis. Sed diam enim, sollicitudin at orci id, venenatis fringilla neque. Etiam pellentesque eget purus eu ultricies. Praesent tristique elit mauris, nec rutrum elit condimentum et. Ut vel lectus eros. Morbi auctor sit amet leo eu lacinia. Duis mattis quam non consequat porttitor. Sed sodales sit amet libero vitae eleifend.',
-            'small',
-          ),
-        ]),
-        SectionLayoutBuilder.build('primary', 'projects', [
-          TextControlBuilder.build('primary', 'Header 3', 'h3'),
-          TextControlBuilder.build(
-            'primary',
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ornare sit amet justo quis porttitor. Vivamus volutpat, velit in cursus tempor, enim lectus viverra augue, non bibendum leo nisl non arcu. Aenean dapibus metus tellus, at viverra ipsum posuere quis. Sed diam enim, sollicitudin at orci id, venenatis fringilla neque. Etiam pellentesque eget purus eu ultricies. Praesent tristique elit mauris, nec rutrum elit condimentum et. Ut vel lectus eros. Morbi auctor sit amet leo eu lacinia. Duis mattis quam non consequat porttitor. Sed sodales sit amet libero vitae eleifend.',
-            'p',
-          ),
-          TextControlBuilder.build(
-            'primary',
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ornare sit amet justo quis porttitor. Vivamus volutpat, velit in cursus tempor, enim lectus viverra augue, non bibendum leo nisl non arcu. Aenean dapibus metus tellus, at viverra ipsum posuere quis. Sed diam enim, sollicitudin at orci id, venenatis fringilla neque. Etiam pellentesque eget purus eu ultricies. Praesent tristique elit mauris, nec rutrum elit condimentum et. Ut vel lectus eros. Morbi auctor sit amet leo eu lacinia. Duis mattis quam non consequat porttitor. Sed sodales sit amet libero vitae eleifend.',
-            'small',
-          ),
-        ]),
-        SectionLayoutBuilder.build('primary', 'education', [
-          TextControlBuilder.build('primary', 'Header 3', 'h3'),
-          TextControlBuilder.build(
-            'primary',
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ornare sit amet justo quis porttitor. Vivamus volutpat, velit in cursus tempor, enim lectus viverra augue, non bibendum leo nisl non arcu. Aenean dapibus metus tellus, at viverra ipsum posuere quis. Sed diam enim, sollicitudin at orci id, venenatis fringilla neque. Etiam pellentesque eget purus eu ultricies. Praesent tristique elit mauris, nec rutrum elit condimentum et. Ut vel lectus eros. Morbi auctor sit amet leo eu lacinia. Duis mattis quam non consequat porttitor. Sed sodales sit amet libero vitae eleifend.',
-            'p',
-          ),
-          TextControlBuilder.build(
-            'primary',
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ornare sit amet justo quis porttitor. Vivamus volutpat, velit in cursus tempor, enim lectus viverra augue, non bibendum leo nisl non arcu. Aenean dapibus metus tellus, at viverra ipsum posuere quis. Sed diam enim, sollicitudin at orci id, venenatis fringilla neque. Etiam pellentesque eget purus eu ultricies. Praesent tristique elit mauris, nec rutrum elit condimentum et. Ut vel lectus eros. Morbi auctor sit amet leo eu lacinia. Duis mattis quam non consequat porttitor. Sed sodales sit amet libero vitae eleifend.',
-            'small',
-          ),
-        ]),
+      MainLayoutBuilder.build([
+        SectionLayoutBuilder.build('about-me', []),
+        SectionLayoutBuilder.build('technologies', []),
+        SectionLayoutBuilder.build('skills', []),
+        SectionLayoutBuilder.build('experience', []),
+        SectionLayoutBuilder.build('resume', []),
+        SectionLayoutBuilder.build('projects', []),
+        SectionLayoutBuilder.build('education', []),
       ]),
-      FooterLayoutBuilder.build('primary', [
-        SectionLayoutBuilder.build('primary', 'contact', [
-          TextControlBuilder.build('primary', 'Header 3', 'h3'),
-          TextControlBuilder.build(
-            'primary',
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ornare sit amet justo quis porttitor. Vivamus volutpat, velit in cursus tempor, enim lectus viverra augue, non bibendum leo nisl non arcu. Aenean dapibus metus tellus, at viverra ipsum posuere quis. Sed diam enim, sollicitudin at orci id, venenatis fringilla neque. Etiam pellentesque eget purus eu ultricies. Praesent tristique elit mauris, nec rutrum elit condimentum et. Ut vel lectus eros. Morbi auctor sit amet leo eu lacinia. Duis mattis quam non consequat porttitor. Sed sodales sit amet libero vitae eleifend.',
-            'p',
-          ),
-          TextControlBuilder.build(
-            'primary',
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ornare sit amet justo quis porttitor. Vivamus volutpat, velit in cursus tempor, enim lectus viverra augue, non bibendum leo nisl non arcu. Aenean dapibus metus tellus, at viverra ipsum posuere quis. Sed diam enim, sollicitudin at orci id, venenatis fringilla neque. Etiam pellentesque eget purus eu ultricies. Praesent tristique elit mauris, nec rutrum elit condimentum et. Ut vel lectus eros. Morbi auctor sit amet leo eu lacinia. Duis mattis quam non consequat porttitor. Sed sodales sit amet libero vitae eleifend.',
-            'small',
-          ),
-        ]),
-        SectionLayoutBuilder.build('primary', 'copyright', [
-          TextControlBuilder.build('primary', 'Header 3', 'h3'),
-          TextControlBuilder.build(
-            'primary',
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ornare sit amet justo quis porttitor. Vivamus volutpat, velit in cursus tempor, enim lectus viverra augue, non bibendum leo nisl non arcu. Aenean dapibus metus tellus, at viverra ipsum posuere quis. Sed diam enim, sollicitudin at orci id, venenatis fringilla neque. Etiam pellentesque eget purus eu ultricies. Praesent tristique elit mauris, nec rutrum elit condimentum et. Ut vel lectus eros. Morbi auctor sit amet leo eu lacinia. Duis mattis quam non consequat porttitor. Sed sodales sit amet libero vitae eleifend.',
-            'p',
-          ),
-          TextControlBuilder.build(
-            'primary',
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ornare sit amet justo quis porttitor. Vivamus volutpat, velit in cursus tempor, enim lectus viverra augue, non bibendum leo nisl non arcu. Aenean dapibus metus tellus, at viverra ipsum posuere quis. Sed diam enim, sollicitudin at orci id, venenatis fringilla neque. Etiam pellentesque eget purus eu ultricies. Praesent tristique elit mauris, nec rutrum elit condimentum et. Ut vel lectus eros. Morbi auctor sit amet leo eu lacinia. Duis mattis quam non consequat porttitor. Sed sodales sit amet libero vitae eleifend.',
-            'small',
-          ),
-        ]),
+      FooterLayoutBuilder.build([
+        SectionLayoutBuilder.build('contact', []),
+        SectionLayoutBuilder.build('copyright', []),
       ]),
     ]),
   ];
