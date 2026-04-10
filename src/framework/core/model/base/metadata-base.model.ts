fa220a458eb6a6c157a91e8010c0290b50ae04cf
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2026-04-10 15:27:35 +0200

    Remove everything from metadata

--- src/framework/core/model/base/metadata-base.model.ts
+++ src/framework/core/model/base/metadata-base.model.ts
@@ -1,11 +1,1 @@
-import { ColorElementType, ColorModifierType } from '../../../mapping/constant/color.type';
-import { FontElementType, FontModifierType } from '../../../mapping/constant/font.type';
-import { SizeElementType, SizeModifierType } from '../../../mapping/constant/size.type';
-import { WeightElementType, WeightModifierType } from '../../../mapping/constant/weight.type';
-
-export interface MetadataBaseModel {
-  color: Record<ColorElementType, ColorModifierType>;
-  font: Record<FontElementType, FontModifierType>;
-  size: Record<SizeElementType, SizeModifierType>;
-  weight: Record<WeightElementType, WeightModifierType>;
-}
+export interface MetadataBaseModel {}
